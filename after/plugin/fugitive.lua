f24ed2446b2e435835d77bb1847fbedd08602fb5
Alvaro Mendoza Alcala <alvaro.mendozaalcala@astrazeneca.com>
2025-08-07 16:42:49 +0200

    Add remap to open diff in vertical

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,5 +1,6 @@
 vim.keymap.set("n", "<leader>gs", ":Git status<CR>")
 vim.keymap.set("n", "<leader>gd", ":Git diff<CR>")
+vim.keymap.set("n", "<leader>gvd", ":vertical Git diff <CR>")
 vim.keymap.set("n", "<leader>gD", ":Git diff --staged<CR>")
 vim.keymap.set("n", "<leader>gl", ":vertical Git log --all --decorate --oneline --graph<CR>")
 vim.keymap.set("n", "<leader>gc", ":Git commit -v<CR>")
