f197a8ccff62a8ad51f339a18506dd0882b2a03f
Alvaro Mendoza Alcala <alvaro.mendozaalcala@astrazeneca.com>
2025-09-03 11:44:45 +0200

    Ensure folder for screenshots exists

--- after/plugin/codesnap.lua
+++ after/plugin/codesnap.lua
@@ -1,8 +1,10 @@
+os.execute('mkdir -p "' .. os.getenv("HOME") .. "/Pictures/codesnap" .. '"')
 require("codesnap").setup({
     watermark = "",
     bg_padding = 0,
     has_line_number = true,
     min_width = 42,
-    save_path = os.getenv("HOME") .. "/Pictures/codesnap"
+    save_path = os.getenv("HOME") .. "/Pictures/codesnap",
+    show_workspace = true,
 })
     
